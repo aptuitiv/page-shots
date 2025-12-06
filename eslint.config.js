43cf6762d53b9e42ebefbe987d83270b91f37e0c
Eric Tompkins <eric@aptuitiv.com>
2025-12-06 11:11:47 -0500

    Include typescript eslint rules and import resolvers

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,14 @@
 import aptuitivEslint from '@aptuitiv/eslint-config-aptuitiv';
 // eslint-disable-next-line import-x/no-extraneous-dependencies -- This library is only used for development so it should be in devDependencies
 import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from 'typescript-eslint';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript'
 
 export default defineConfig([
     // Because @aptuitiv/eslint-config-aptuitiv exports an array of objects, we need to use the spread operator.
     ...aptuitivEslint,
+    // Custom TypeScript ESLint rules
+    ...tseslint.configs.recommended,
     // Ignore the docs directory
     globalIgnores(['docs/',]),
     // Custom rules for this project
@@ -20,5 +24,13 @@ export default defineConfig([
             // Allow importing Javascript files
             'import-x/extensions': ['off'],
         },
+        settings: {
+            'import-x/resolver-next': [
+                createTypeScriptImportResolver({
+                    // Always try to resolve types under `<root>@types` directory even if it doesn't contain any source code, like `@types/unist`
+                    alwaysTryTypes: true,
+                }),
+            ],
+        }
     },
 ]);
